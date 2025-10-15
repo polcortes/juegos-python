dda912c65e1ebf6eb4b4bdf281c0c2bd4c3bcfd1
Pol Cortés <polcortesg@gmail.com>
2025-10-15 17:18:44 +0200

    feat: implement game logic and winner detection in tres en ralla

--- tresEnRalla/main.py
+++ tresEnRalla/main.py
@@ -1,3 +1,6 @@
+from utils.console import clear_console
+
+
 WINNER_COMBOS: list[list[int]] = [
     [0, 1, 2],
     [3, 4, 5],
@@ -28,7 +31,13 @@ def print_board():
     print("  ".join(board[6:]), end="")
     print((" " * 10) + "6  7  8")
 
-def get_cell(who="X"):
+def check_winner(mark):
+    for combo in WINNER_COMBOS:
+        if all(board[i] == mark for i in combo):
+            return True
+    return False
+
+def get_cell(mark):
     idx = input("¿Qué casilla quieres marcar? (En el tablero de la derecha se muestran los índices de cada casilla)")
 
     try:
@@ -39,6 +48,8 @@ def get_cell(who="X"):
 
         if board[idx] != "·":
             raise AlreadyMarkedCellError
+        
+        board[idx] = mark
     except ValueError:
         print("Carácter inválido...")
         get_cell()
@@ -48,14 +59,47 @@ def get_cell(who="X"):
     except AlreadyMarkedCellError:
         print("Esa casilla ya ha sido marcada...")
 
+def ask_play_again():
+    response = input("¿Quieres jugar otra vez? (y/Y = Sí, n/N = No): ").strip().lower()
+    if response == 'y':
+        play()
+    elif response == 'n':
+        print("Gracias por jugar. ¡Hasta la próxima!")
+    else:
+        print("Respuesta inválida. Por favor, responde con 's' o 'n'.")
+        ask_play_again()
+
 def play():
-    rounds = 1
+    global board
+    board = ["·", "·", "·", "·", "·", "·", "·", "·", "·"]
+    clear_console()
     while True:
-        print(f"--- RONDA {rounds} ----------------------------------")
         print("Turno de Jugador 1 (X)")
         print_board()
-        idx = input("¿Qué casilla quieres marcar?")
         
+        get_cell(PLAYER_ONE_SYMBOL)
+        if check_winner(PLAYER_ONE_SYMBOL):
+            print_board()
+            print("¡Ha ganado el Jugador 1!")
+            ask_play_again()
+            break
+
+        # Si check_winner devuelve False, comprobamos que el tablero siga teniendo casillas libres
+        if all(cell != "·" for cell in board):
+            print_board()
+            print("¡Empate!")
+            ask_play_again()
+            break
+
+        print("Turno de Jugador 2 (O)")
+        print_board()
+        get_cell(PLAYER_TWO_SYMBOL)
+
+        if check_winner(PLAYER_TWO_SYMBOL):
+            print_board()
+            print("¡Ha ganado el Jugador 2!")
+            ask_play_again()
+            break
 
 
 if __name__ == "__main__":
