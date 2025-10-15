26f616cfee3afd12cfffa94149b0fac659714d54
Pol Cortés <polcortesg@gmail.com>
2025-10-15 17:18:22 +0200

    refactor: move clear_console function to utils.console module

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ from blackjack import play as play_blackjack
 from hundirLaFlota import play as play_hundir_la_flota
 from tresEnRalla import play as play_tres_en_ralla
 from typing import Callable
-import os
-
-
-def clear_console():
-    os.system('cls' if os.name=='nt' else 'clear')
+from utils.console import clear_console
 
 
 opts: dict[str, Callable] = {
@@ -23,13 +19,14 @@ while True:
         "¿A qué quieres jugar?\n" + \
         "    0. Blackjack\n" + \
         "    1. Hundir la flota\n" + \
+        "    2. Tres en raya\n" + \
         "> "
     )
 
     opt: Callable | None = opts.get(response)
 
     if opt is None:
-        print("Opción inválida...\n")
+        input("Opción inválida, presiona Enter para continuar...\n")
         continue
 
     opt()
